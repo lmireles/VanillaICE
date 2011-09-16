7f7c36ba1093bff6c714964ba2751ac663671c21
rscharpf@jhsph.edu <rscharpf@jhsph.edu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-09-16 02:32:35 +0000

    Add hmm method for CNSet object.
    This coerces to an oligoSet prior to fitting the HMM. We're smoothing point estimates of total copy number, rather than fitting a hmm to the prediction regions
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VanillaICE@58114 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-CNSet.R
+++ R/methods-CNSet.R
@@ -1,50 +1,106 @@
-setMethod("hmm2", signature(object="CNSet", hmm.params="HmmOptionList"),
+##setMethod("hmm2", signature(object="CNSet", hmm.params="HmmOptionList"),
+##	  function(object, hmm.params, ...){
+##		  sample.index <- sampleIndex(hmm.params)
+##		  marker.index <- markerIndex(hmm.params)
+##		  v <- verbose(hmm.params)
+##		  if(is.null(sample.index)){
+##			  if(v > 0) message("sample.index is NULL.  Using all samples")
+##			  sample.index <- seq(length=ncol(object))
+##		  }
+##		  sample.index.list <- splitIndicesByLength(sample.index, ocSamples())
+##		  if(is.null(marker.index)){
+##			  if(v > 0) message("marker.index is NULL.  Using all markers")
+##			  marker.index <- seq(length=nrow(object))
+##		  }
+##		  marker.index.list <- split(marker.index, chromosome(object)[marker.index])
+##		  oligoClasses:::open(object$SNR)
+##		  snr <- object$SNR[]
+##		  if(any(snr < 5)) warning(sum(snr < 5), " samples have SNR < 5.  May want to exclude these samples by specifying sample.index.")
+##		  oligoClasses:::open(object$SNR)
+##		  rm(snr)
+##		  fit <- vector("list", length(marker.index.list)*length(sample.index.list))
+##		  k <- 1
+##		  oligoClasses:::open(object)
+##		  for(i in seq_along(marker.index.list)){
+##			  ii <- marker.index.list[[i]]
+##			  for(j in seq_along(sample.index.list)){
+##				  jj <- sample.index.list[[j]]
+##				  cnset <- object[ii, jj]
+##				  cnset <- cnset[order(position(cnset)), ]
+##				  oligoSet <- as(cnset, "oligoSnpSet")
+##				  oligoSet <- centerCopyNumber(oligoSet, ...)
+##				  sds <- robustSds2(copyNumber(oligoSet), ...)
+##				  cnConfidence(oligoSet) <- 1/sds
+##				  rm(sds, cnset); gc()
+##				  fit[[k]] <- hmm(oligoSet, hmm.params, ...)
+##				  k <- k+1
+##			  }
+##		  }
+##		  oligoClasses:::close(object)
+##		  if(length(fit) > 1){
+##			  rd <- RangedDataList(fit)
+##			  rd2 <- stack(rd)
+##			  rm(fit,rd); gc()
+##			  ix <- match("sample", colnames(rd2))
+##			  if(length(ix) > 0) rd2 <- rd2[, -ix]
+##		  } else rd2 <- fit[[1]]
+##		  return(rd2)
+##	  })
+setMethod("hmm", signature(object="CNSet", hmm.params="HmmOptionList"),
 	  function(object, hmm.params, ...){
-		  sample.index <- sampleIndex(hmm.params)
-		  marker.index <- markerIndex(hmm.params)
-		  v <- verbose(hmm.params)
-		  if(is.null(sample.index)){
-			  if(v > 0) message("sample.index is NULL.  Using all samples")
-			  sample.index <- seq(length=ncol(object))
+		  if("verbose" %in% names(list(...))){
+			  verbose <- list(...)[["verbose"]]
+		  } else verbose <- TRUE
+		  if("by.chromosome" %in% names(list(...))){
+			  by.chromosome <- list(...)[["by.chromosome"]]
+		  } else by.chromosome <- TRUE
+		  if("k" %in% names(list(...))){
+			  by.chromosome <- list(...)[["k"]]
+		  } else k <- 5
+		  if("sample.index" %in% names(list(...))){
+			  sample.index <- list(...)[["sample.index"]]
+		  } else sample.index <- seq(length=ncol(object))
+		  batch.index <- split(sample.index, batch(object)[sample.index])
+		  hmm.params[["verbose"]] <- 0L
+		  chrom <- unique(chromosome(object))
+		  NN <- length(chrom) * length(batch.index)
+		  autosome.index <- which(chromosome(object) < 23)
+		  if(verbose){
+			  pb <- txtProgressBar(min=0, max=NN, style=3)
 		  }
-		  sample.index.list <- splitIndicesByLength(sample.index, ocSamples())
-		  if(is.null(marker.index)){
-			  if(v > 0) message("marker.index is NULL.  Using all markers")
-			  marker.index <- seq(length=nrow(object))
-		  }
-		  marker.index.list <- split(marker.index, chromosome(object)[marker.index])
-		  oligoClasses:::open(object$SNR)
-		  snr <- object$SNR[]
-		  if(any(snr < 5)) warning(sum(snr < 5), " samples have SNR < 5.  May want to exclude these samples by specifying sample.index.")
-		  oligoClasses:::open(object$SNR)
-		  rm(snr)
-		  fit <- vector("list", length(marker.index.list)*length(sample.index.list))
-		  k <- 1
-		  oligoClasses:::open(object)
-		  for(i in seq_along(marker.index.list)){
-			  ii <- marker.index.list[[i]]
-			  for(j in seq_along(sample.index.list)){
-				  jj <- sample.index.list[[j]]
-				  cnset <- object[ii, jj]
-				  cnset <- cnset[order(position(cnset)), ]
-				  oligoSet <- as(cnset, "oligoSnpSet")
-				  oligoSet <- centerCopyNumber(oligoSet, ...)
-				  sds <- robustSds2(copyNumber(oligoSet), ...)
-				  cnConfidence(oligoSet) <- 1/sds
-				  rm(sds, cnset); gc()
-				  fit[[k]] <- hmm(oligoSet, hmm.params, ...)
-				  k <- k+1
+		  results <- vector("list", NN)
+		  m <- 1
+		  for(i in seq_along(batch.index)){
+			  for(j in seq_along(chrom)){
+				  CHR <- chrom[j]
+				  is.autosome <- CHR < 23
+				  J <- batch.index[[i]]
+				  I <- which(chromosome(object) == CHR)
+				  cnset.batch <- object[I, J]
+				  oligoSet <- as(cnset.batch, "oligoSnpSet")
+				  oligoSet <- oligoSet[order(position(oligoSet)), ]
+				  rm(cnset.batch)
+				  gc()
+				  cn <- copyNumber(oligoSet)
+				  if(is.autosome){
+					  sds <- apply(cn, 2, mad, na.rm=TRUE)
+				  }
+				  cnConfidence(oligoSet) <- matrix(1/sds, nrow(oligoSet), ncol(oligoSet),
+								   byrow=TRUE)
+				  rm(cn, sds)
+				  oligoSet <- centerAutosomesAt(oligoSet, at=2)
+				  hmmOpts <- HmmOptionList(object=oligoSet, verbose=0L)
+				  results[[m]] <- hmm(oligoSet, hmmOpts, k=k)
+				  if(verbose) setTxtProgressBar(pb, m)
+				  m <- m+1
 			  }
 		  }
-		  oligoClasses:::close(object)
-		  if(length(fit) > 1){
-			  rd <- RangedDataList(fit)
-			  rd2 <- stack(rd)
-			  rm(fit,rd); gc()
-			  ix <- match("sample", colnames(rd2))
-			  if(length(ix) > 0) rd2 <- rd2[, -ix]
-		  } else rd2 <- fit[[1]]
-		  return(rd2)
+		  if(verbose) close(pb)
+		  tmp <- stack(RangedDataList(results))
+		  index <- match("sample", colnames(tmp))
+		  if(length(index) == 1) tmp <- tmp[, -index]
+		  rangedData <- tmp
+		  return(rangedData)
 	  })
 
 setMethod("xyplot", signature(x="formula", data="CNSet"),
