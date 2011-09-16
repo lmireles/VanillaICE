04151081934be62e35e94ecb43f67aa6508bc39a
rscharpf@jhsph.edu <rscharpf@jhsph.edu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-09-16 02:32:53 +0000

    Add hmm method for CopyNumberSet and SnpSet
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VanillaICE@58117 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/methods-CopyNumberSet.R
@@ -0,0 +1,67 @@
+setMethod("hmm2", signature(object="CopyNumberSet", hmm.params="HmmOptionList"),
+	  function(object, hmm.params, ...){
+		  verbose <- hmm.params[["verbose"]] > 0
+		  log.beta <- cnEmission(object, hmm.params, verbose=verbose, ...)
+		  dimnames(log.beta) <- list(featureNames(object),
+					     sampleNames(object),
+					     hmm.params$states)
+		  res <- viterbi(object, hmm.params, log.E=log.beta)
+		  return(res)
+})
+
+setMethod("hmm", signature(object="CopyNumberSet", hmm.params="HmmOptionList"),
+	  function(object, hmm.params, ...){
+		  v2 <- hmm.params$verbose
+		  naindex <- which(is.na(chromosome(object)) | is.na(position(object)))
+		  if(length(naindex) > 0)
+			  object <- object[-naindex, ]
+		  if(v2 > 0){
+			  if("k" %in% names(list(...))){
+				  k <- list(...)[["k"]]
+			  } else k <- 3
+			  message("Using a running median of ", k, " markers to estimate the outlier probability.")
+		  }
+		  marker.index.list <- split(seq(length=nrow(object)), chromosome(object))
+		  chromosomes <- unique(chromosome(object))
+		  ix <- order(chromosome(object), position(object))
+		  if(any(diff(ix) < 0)) {
+			  object <- object[ix, ]
+		  }
+		  if(is.null(hmm.params$sample.index)){
+			  sample.index <- seq(length=ncol(object))
+		  } else sample.index <- hmm.params$sample.index
+		  if(v2 > 0) {
+			  pb <- txtProgressBar(min=0, max=ncol(object), style=3)
+		  }
+		  res <- vector("list", ncol(object))
+		  for(j in seq_along(sample.index)){
+			  jj <- sample.index[j]
+			  tmp <- vector("list", length(chromosomes))
+			  for(k in seq_along(chromosomes)){
+				  CHR <- chromosomes[k]
+				  i <- marker.index.list[[k]]
+				  obj <- object[i, jj]
+				  tmp[[k]] <- hmm2(object=obj, hmm.params=hmm.params, ...)
+			  }
+			  if(length(tmp) > 1){
+				  rdlist <- RangedDataList(tmp)
+				  rd <- stack(rdlist)
+				  ix <- match("sample", colnames(rd))
+				  if(length(ix) > 0) rd <- rd[, -ix]
+				  rm(rdlist)
+			  } else rd <- tmp[[1]]
+			  res[[j]] <- rd
+			  rm(tmp, rd); gc()
+			  if(v2 > 0) setTxtProgressBar(pb, j)
+		  }
+		  if(v2 > 0) close(pb)
+		  if(length(res) > 1){
+			  ##rdlist <- lapply(res, function(x) as(x, "RangedData"))
+			  rdlist <- RangedDataList(res)
+			  rd <- stack(rdlist)
+			  ix <- match("sample", colnames(rd))
+			  if(length(ix) > 0) rd <- rd[, -ix]
+			  rm(rdlist)
+		  } else rd <- res[[1]]
+		  return(rd)
+	  })
---
+++ R/methods-SnpSet.R
@@ -0,0 +1,70 @@
+setMethod("hmm2", signature(object="SnpSet", hmm.params="HmmOptionList"),
+	  function(object, hmm.params, ...){
+		  log.beta <- gtEmission(object, hmm.params)
+		  dimnames(log.beta) <- list(featureNames(object),
+					     sampleNames(object),
+					     hmm.params$states)
+		  viterbi(object, hmm.params, log.E=log.beta)
+})
+
+setMethod("hmm", signature(object="SnpSet", hmm.params="HmmOptionList"),
+	  function(object, hmm.params, ...){
+		  naindex <- which(is.na(chromosome(object)) | is.na(position(object)))
+		  if(length(naindex) > 0)
+			  object <- object[-naindex, ]
+		  v2 <- hmm.params$verbose
+		  naindex <- which(is.na(chromosome(object)) | is.na(position(object)))
+		  ice <- hmm.params$ICE
+		  if(ice) checkAnnotation(object)
+		  if(length(naindex) > 0){
+			  warning("NA's in annotation -- check chromosome or position. Ignoring markers with missing annotation")
+			  object <- object[-naindex, ]
+		  }
+		  missingGT <- any(is.na(calls(object)))
+		  if(missingGT){
+			  if(v2 > 0) message("Some genotypes are NAs.  The default assumes that prGenotypeMissing is the same for each state -- see hmmOptions")
+		  }
+		  marker.index.list <- split(seq(length=nrow(object)), chromosome(object))
+		  chromosomes <- unique(chromosome(object))
+		  ix <- order(chromosome(object), position(object))
+		  if(any(diff(ix) < 0)) {
+			  object <- object[ix, ]
+		  }
+		  if(is.null(hmm.params$sample.index)){
+			  sample.index <- seq(length=ncol(object))
+		  } else sample.index <- hmm.params$sample.index
+		  if(v2 > 0) {
+			  pb <- txtProgressBar(min=0, max=ncol(object), style=3)
+		  }
+		  res <- vector("list", ncol(object))
+		  for(j in seq_along(sample.index)){
+			  jj <- sample.index[j]
+			  tmp <- vector("list", length(chromosomes))
+			  for(k in seq_along(chromosomes)){
+				  CHR <- chromosomes[k]
+				  i <- marker.index.list[[k]]
+				  obj <- object[i, jj]
+				  tmp[[k]] <- hmm2(object=obj, hmm.params=hmm.params, ...)
+			  }
+			  if(length(tmp) > 1){
+				  rdlist <- RangedDataList(tmp)
+				  rd <- stack(rdlist)
+				  ix <- match("sample", colnames(rd))
+				  if(length(ix) > 0) rd <- rd[, -ix]
+				  rm(rdlist)
+			  } else rd <- tmp[[1]]
+			  res[[j]] <- rd
+			  rm(tmp, rd); gc()
+			  if(v2 > 0) setTxtProgressBar(pb, j)
+		  }
+		  if(v2 > 0) close(pb)
+		  if(length(res) > 1){
+			  ##rdlist <- lapply(res, function(x) as(x, "RangedData"))
+			  rdlist <- RangedDataList(res)
+			  rd <- stack(rdlist)
+			  ix <- match("sample", colnames(rd))
+			  if(length(ix) > 0) rd <- rd[, -ix]
+			  rm(rdlist)
+		  } else rd <- res[[1]]
+		  return(rd)
+	  })
