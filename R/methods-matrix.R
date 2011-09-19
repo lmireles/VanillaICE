78aa684bf9fd286255eb8d8f7fc1f67052132ffd
rscharpf@jhsph.edu <rscharpf@jhsph.edu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-09-19 17:20:06 +0000

    Add methods for matrix -- gtEmission and cnEmission
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VanillaICE@58201 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/methods-matrix.R
@@ -0,0 +1,78 @@
+setMethod("cnEmission", signature(object="matrix", stdev="matrix"),
+	  function(object, stdev, k=5, cnStates, is.log=FALSE, ...){
+		  stopifnot(all(dim(object) == dim(stdev)))
+		  if(any(colSums(is.na(object)) == nrow(object))){
+			  stop("Some samples have all missing values. Exclude these samples before continuing.")
+		  }
+		  S <- length(cnStates)
+		  emission.cn <- array(NA, dim=c(nrow(object), ncol(object), S))
+		  if(is.log){
+			  MIN.CN <- -10
+			  MAX.CN <- 2.5
+		  } else {
+			  MIN.CN <- 0
+			  MAX.CN <- 10
+		  }
+		  if(any(object < MIN.CN, na.rm=TRUE)) object[object < MIN.CN] <- MIN.CN
+		  if(any(object > MAX.CN, na.rm=TRUE)) object[object > MAX.CN] <- MAX.CN
+		  for(j in 1:ncol(object)){
+			  cn <- object[, j, drop=FALSE]
+			  s <- stdev[, j, drop=FALSE]
+			  I <- which(!is.na(as.numeric(cn)))
+			  old.tmp <- tmp <- rep(NA, length(as.numeric(cnStates)))
+			  cnvector <- as.numeric(cn)[I]
+			  prOutlier <- probabilityOutlier(cnvector, k=k)
+			  for(l in seq_along(cnStates)){
+				  mu <- cnStates[l]
+				  tmp <- (1-prOutlier) * dnorm(x=cnvector,
+							       mean=cnStates[l],
+							       sd=as.numeric(s)[I]) +
+								       prOutlier * dunif(cnvector, MIN.CN, MAX.CN)
+				  emission.cn[I, j, l] <- tmp
+			  }
+		  }
+		  return(log(emission.cn))
+	  })
+
+setMethod("gtEmission", signature(object="matrix"),
+	  function(object, hmmOptions, gt.conf, is.snp, cdfName, ...){
+		  ICE <- hmmOptions$ICE
+		  states <- hmmOptions$states
+		  if(!ICE){
+			  p <- hmmOptions$prGtHom
+			  prGenotypeMissing <- hmmOptions$prGtMis
+			  verbose <- hmmOptions$verbose
+			  stopifnot(length(p) == length(states))
+			  if(!is.numeric(object)) stop("genotypes must be integers (1=AA, 2=AB, 3=BB) or NA (missing)")
+
+			  emission <- array(object, dim=c(nrow(object), ncol(object), length(states))) ##dimnames=list(featureNames(object), sampleNames(object), states))
+			  missingGT <- any(is.na(object))
+			  for(s in seq(along=states)){
+				  tmp <- object
+				  tmp[tmp == 1 | tmp == 3] <- p[s]
+				  tmp[tmp == 2] <- 1-p[s]
+				  index1 <- is.na(tmp) & !is.snp##!isSnp(object)
+				  index2 <- is.na(tmp) & is.snp##isSnp(object)
+				  if(missingGT){
+					  tmp[index2] <- prGenotypeMissing[s]
+					  tmp[index1] <- 1/length(states)
+				  }
+				  emission[, , s] <- tmp
+			  }
+			  logemit <- log(emission)
+			  ##return(logemit)
+		  } else {
+			  ##stop('need to update ICE option')
+			  logemit <- array(NA, dim=c(nrow(object), ncol(object), length(states)))
+			  tmp <- genotypeEmissionCrlmm(object, hmmOptions=hmmOptions, gt.conf=gt.conf, cdfName=cdfName)
+			  rohStates <- which(hmmOptions[["rohStates"]])
+			  notRohState <- which(!hmmOptions[["rohStates"]])
+			  if(length(rohStates) > 0){
+				  logemit[, , rohStates] <- tmp[, , "ROH"]
+			  }
+			  if(length(notRohState) > 0){
+				  logemit[, , notRohState] <- tmp[, , "normal"]
+			  }
+		  }
+		  return(logemit)
+	  })
